ff044251d7f8db3bcad3f37f477e558c63712e48
Tomasz Szarstuk <szarsti@gmail.com>
2016-04-12 17:21:48 +0100

    Add another unit test for adding bit numbers that casue bigger than 2 digit overflow

--- c_test/dec_math_tests.c
+++ c_test/dec_math_tests.c
@@ -9,7 +9,7 @@ void test_adding_simple_intergers() {
   decimal a = {.coefficient = 1, .exponent = 0};
   decimal b = {.coefficient = 2, .exponent = 0};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(3, sum.coefficient);
+  assert_long_equal(3, sum.coefficient);
   assert_int_equal(0, sum.exponent);
 }
 
@@ -17,7 +17,7 @@ void test_adding_floats() {
   decimal a = {.coefficient = 125, .exponent = -2};
   decimal b = {.coefficient = 21, .exponent = 0};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(2225, sum.coefficient);
+  assert_long_equal(2225, sum.coefficient);
   assert_int_equal(-2, sum.exponent);
 }
 
@@ -25,7 +25,7 @@ void test_adding_with_overflow () {
   decimal a = {.coefficient = 93000000000000, .exponent = -14};
   decimal b = {.coefficient = 93000000000000, .exponent = -14};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(18600000000000, sum.coefficient);
+  assert_long_equal(18600000000000, sum.coefficient);
   assert_int_equal(-13, sum.exponent);
 }
 
@@ -41,7 +41,7 @@ void test_adding_int_to_negative_float () {
   decimal a = {.coefficient = -10000000000000, .exponent = -13};
   decimal b = {.coefficient = 2, .exponent = 0};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(10000000000000, sum.coefficient);
+  assert_long_equal(10000000000000, sum.coefficient);
   assert_int_equal(-13, sum.exponent);
 }
 
@@ -49,7 +49,7 @@ void test_adding_float_to_zero () {
   decimal a = {.coefficient = 0, .exponent = 0};
   decimal b = {.coefficient = 10000000000000, .exponent = -14};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(10000000000000, sum.coefficient);
+  assert_long_equal(10000000000000, sum.coefficient);
   assert_int_equal(-14, sum.exponent);
 }
 
@@ -57,8 +57,8 @@ void test_adding_int_to_float () {
   decimal a = {.coefficient = 100000000000000, .exponent = -14};
   decimal b = {.coefficient = 14073, .exponent = 0};
   decimal sum = dec_add(a, b);
-  assert_ulong_equal(140740000000000, sum.coefficient);
-  assert_int_equal(-10, sum.exponent);
+  assert_long_equal(14074000000000, sum.coefficient);
+  assert_int_equal(-9, sum.exponent);
 }
 
 void test_adding_int_to_negative_float_with_overflow () {
@@ -69,15 +69,25 @@ void test_adding_int_to_negative_float_with_overflow () {
   assert_int_equal(-11, sum.exponent);
 }
 
+void test_adding_big_ints_with_overflow () {
+  decimal a = {.coefficient = 14073748835533, .exponent = 2};
+  decimal b = {.coefficient = 156374987061480, .exponent = 0};
+  decimal sum = dec_add(a, b);
+  assert_long_equal(15637498706147, sum.coefficient);
+  assert_int_equal(2, sum.exponent);
+}
+
 //
 // dec_div_tests
 //
 void test_dividing () {
   decimal a = {.coefficient = 18600000000000, .exponent = -13};
   decimal r = dec_div(a, 2);
-  assert_ulong_equal(9300000000000, r.coefficient);
+  assert_long_equal(9300000000000, r.coefficient);
   assert_int_equal(-13, r.exponent);
 }
+
+uint8_t qlog10(uint64_t v);
 
 void dec_math_tests() {
   test_fixture_start();
@@ -89,6 +99,7 @@ void dec_math_tests() {
   run_test(test_adding_float_to_zero);
   run_test(test_adding_int_to_float);
   run_test(test_adding_int_to_negative_float_with_overflow);
+  run_test(test_adding_big_ints_with_overflow);
   run_test(test_dividing);
   test_fixture_end();
 }
